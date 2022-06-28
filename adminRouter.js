504aa15838fbf06c2fd62c8a1dec1afe9ba568e7
SharedPreferences <boothaganesh.jdev@gmail.com>
2022-06-28 18:32:46 +0530

    Working on getAllData

---
+++ adminRouter.js
@@ -0,0 +1,229 @@
+import "dotenv/config";
+import express from "express";
+import generateUniqueId from "generate-unique-id";
+import salwarSchema from "./schema/salwarSchema.js";
+import blouseSchema from "./schema/blouseSchema.js";
+import userCreationalSchema from "./schema/userCreationalSchema.js";
+import emailDetailSchema from "./schema/emailCreationalSchema.js";
+import shirtSchema from "./schema/shirtSchema.js";
+import pantSchema from "./schema/pantSchema.js";
+import mongoose from "mongoose";
+import randomColor from "randomcolor";
+
+import registerSchema from "./schema/registerSchema.js";
+
+import jwt from "jsonwebtoken";
+
+const TOKEN_KEY = "STONER";
+
+const router = express.Router();
+router.use(express.json());
+
+var Admin = mongoose.mongo.Admin;
+
+router.get("/", (req, res) => {
+  res.json("admin router works");
+});
+
+router.post("/getAllDataBase", async (req, res) => {
+  var requestData = req.body;
+  let dbName = requestData.username.split("@")[0] + "SmartTailorShopDB";
+  try {
+    mongoose.disconnect();
+
+    var allDBNames = [];
+
+    var dataToRes = [];
+
+    let mongoURL = "mongodb://localhost/" + dbName;
+
+    var connection = await mongoose.createConnection(mongoURL);
+
+    const temp = await connection.on("open", function () {
+      new Admin(connection.db).listDatabases(async function (err, result) {
+        var allDatabases = result.databases.map((item, index) => {
+          return item.name;
+        });
+
+        for (let i = 0; i < allDatabases.length; i++) {
+          // console.log(allDatabases[i].split('SmartTailorShopDB'));
+          if (allDatabases[i].split("SmartTailorShopDB").length === 2) {
+            allDBNames.push(
+              allDatabases[i].split("SmartTailorShopDB")[0] +
+                "SmartTailorShopDB"
+            );
+          }
+        }
+
+        for (let i = 0; i < allDBNames.length; i++) {
+          let url = "mongodb://localhost/" + allDBNames[i];
+          //   console.log(url);
+          //   console.log(connection.models);
+          connection.close();
+          const db = await mongoose.connect(url);
+          const omit = { emailId: 1, _id: 0, name: 1 };
+          let foundData = await registerSchema.findOne({}, omit);
+          dataToRes.push(foundData);
+          //   console.log(dataToRes);
+          db.disconnect();
+        }
+
+        var pageNo = req.body.pageNo; // 2
+        let size = req.body.pageSize; // 5
+        var lastPos = 0;
+        var startPos = 0;
+        for (let i = 1; i <= pageNo; i++) {
+          lastPos = i * size;
+        }
+
+        startPos = lastPos - size;
+
+        console.log(`Start Position -> ${startPos}`);
+        console.log(`Last Position -> ${lastPos}`);
+        console.log(`Total Data : ${allDatabases.length}`);
+
+        var tempStore = [];
+
+        for (let i = startPos; i <= lastPos; i++) {
+          console.log(i);
+          if (dataToRes[i] !== null) {
+            tempStore.push(dataToRes[i]);
+          }
+          break
+         
+        }
+
+        // var allStudentData = await studentModal
+        // .find(searchFilter, { _id: 0, __v: 0 })
+        // .skip(skips)
+        // .limit(size)
+        // .lean();
+
+        return res.json({
+          success: true,
+          data: dataToRes,
+          tempStore,
+          count: allDatabases.length,
+        });
+      });
+    });
+  } catch (error) {
+    // console.log(error + "ddd");
+    return res.json({ success: false, message: "Server Down" });
+  }
+});
+
+router.post("/deleteDB", async (req, res) => {
+  mongoose.disconnect();
+  var requestData = req.body.username.split("@")[0];
+  let dbName = requestData.split("@")[0];
+  const url = `mongodb://localhost/${dbName}SmartTailorShopDB`;
+  console.log(requestData);
+  await mongoose.connect(url);
+
+  const connection = mongoose.connection;
+  connection.once("open", () => {
+    console.log("MongoDB database connection established successfully");
+  });
+
+  mongoose.connection.dropDatabase().then(async () => {
+    try {
+      mongoose.connection.close();
+      return res.json({
+        success: true,
+        message: "Database Deleted Successfully.",
+      });
+    } catch (err) {
+      console.log(err);
+      return res.json({ success: true, message: "Unable to Delete databse" });
+    }
+  });
+});
+
+export default router;
+
+/**
+ * 
+ * router.post("/getAllDataBase", async (req, res) => {
+  var requestData = req.body;
+  let dbName = requestData.username.split("@")[0] + "SmartTailorShopDB";
+  try {
+    mongoose.disconnect();
+
+    var allDBNames = [];
+
+    var dataToRes = [];
+
+    let mongoURL = "mongodb://localhost/" + dbName;
+
+    var connection = await mongoose.createConnection(mongoURL);
+
+    const temp = await connection.on("open", function () {
+      new Admin(connection.db).listDatabases(async function (err, result) {
+        var allDatabases = result.databases.map((item, index) => {
+          return item.name;
+        });
+        
+
+        for (let i = 0; i < allDatabases.length; i++) {
+          // console.log(allDatabases[i].split('SmartTailorShopDB'));
+          if (allDatabases[i].split("SmartTailorShopDB").length === 2) {
+            allDBNames.push(
+              allDatabases[i].split("SmartTailorShopDB")[0] +
+                "SmartTailorShopDB"
+            );
+          }
+        }
+
+        for (let i = 0; i < allDBNames.length; i++) {
+          let url = "mongodb://localhost/" + allDBNames[i];
+          console.log(url);
+          console.log(connection.models);
+          connection.close();
+          const db = await mongoose.connect(url);
+          const omit = { emailId: 1, _id: 0, name: 1 };
+          let foundData = await registerSchema.findOne({}, omit);
+          dataToRes.push(foundData);
+          console.log(dataToRes);
+          db.disconnect();
+        }
+
+        var pageNo = req.body.pageNo;
+        let size = req.body.pageSize;
+        var result = 0;
+        for (let i = 1; i <= pageNo; i++) {
+          result = i * size;
+        }
+
+        var skips = dataToRes.length - result;
+        if (skips <= 0) {
+          size = size + skips;
+          skips = 0;
+        }
+
+        console.log(`result`, result);
+        console.log(`skips -> start`, skips);
+        console.log(`size -> end`, size);
+
+        for (let i = 0; i < dataToRes.length; i++) {}
+
+        // var allStudentData = await studentModal
+        // .find(searchFilter, { _id: 0, __v: 0 })
+        // .skip(skips)
+        // .limit(size)
+        // .lean();
+
+        return res.json({
+          success: true,
+          data: dataToRes,
+          size: dataToRes.length,
+        });
+      });
+    });
+  } catch (error) {
+    console.log(error + "ddd");
+    return res.json({ success: false, message: "Server Down" });
+  }
+});
+
+ */
